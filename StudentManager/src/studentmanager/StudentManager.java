5e573242f447897e03795bef7c6089fd9a3fd752
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-09-15 21:26:51 +0700

    add the method addStudent, studentsByGroup, removeStudent

--- StudentManager/src/studentmanager/StudentManager.java
+++ StudentManager/src/studentmanager/StudentManager.java
@@ -1,26 +1,79 @@
 
 package studentmanager;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
-
 public class StudentManager {
 
-    private final Student[] students;
-    private final int countStudent;
+    private Student[] students;
+    private int countStudent;
+    private int countClass;
+    private String[] className;
+    
 
     public StudentManager() {
         this.countStudent = 0;
-        this.students = new Student[10];
-    }
-    public void studentsByGroup() {
-        
+        this.countClass = 0;
+        this.students = new Student[100];
+        this.className = new String[100];
     }
     
+    // Check two student are the same the group
     public boolean sameGroup(Student s1, Student s2) {
-        if (s1.getGroup().equals(s2.getGroup())) return TRUE;
-        else return FALSE;
+        return s1.getGroup().equals(s2.getGroup());
     }
+    
+    public void addStudent(Student s) {
+        if(countStudent<=100) {
+            students[countStudent] = s;
+            countStudent++;
+        }
+        else {
+            System.out.println("Vuot qua so hoc sinh");
+        }
+    }
+    
+    //Show students by group
+    public void studentsByGroup() {
+       
+        for (int index = 0; index < countStudent; index++) {
+            boolean check = true;
+            for (int i = 0; i <= countClass; i++) {
+                if (students[index].getGroup().equals(className[i])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                className[countClass] = students[index].getGroup();
+                countClass++;
+            }
+        }
+        for (int i = 0; i < countClass; i++) {
+            System.out.print("--Lop " + className[i]);
+            System.out.println(":");
+            
+            for (int j = 0; j < countStudent; j++) {
+                if (students[j].getGroup().equals(className[i])) {
+                    System.out.print("* ");
+                    System.out.println(students[j].getInfo());
+                    System.out.println("");
+                }
+            }
+        }
+    }
+
+    //Remove a Student
+    public void removeStudent(String id) {
+        for (int i = 0; i < countStudent; i++) {
+            if (students[i].getId().equals(id)) {
+                for (int j = i; j < countStudent; j++) {
+                    students[j] = students[j + 1];
+                }
+                countStudent--;
+                break;
+            }
+        }
+    }
+    
     
     public static void main(String[] args) {
         // Tao ra 1 doi tuong sinh vien
@@ -39,17 +92,26 @@ public class StudentManager {
         System.out.println(student.getInfo());
         
         
-        Student s1 = new Student();
+        Student s1 = new Student("17020853", "Le Thi Thuy Linh", "17020853@vnu.edu.vn");
         s1.setGroup("K60CA");
         
-        Student s2 = new Student();
+        Student s2 = new Student("17020854", "Le Thi Thuy Linh", "17020854@vnu.edu.vn");
         s2.setGroup("K60CA");
         
-        Student s3 = new Student();
+        Student s3 = new Student("17020922", "Bui Thi Ngat", "17020922@vnu.edu.vn");
         s3.setGroup("K60CB");
         
         StudentManager stdManager = new StudentManager();
         System.out.println(stdManager.sameGroup(s1,s2));
+        
+        stdManager.addStudent(s1);
+        stdManager.addStudent(s2);
+        stdManager.addStudent(s3);
+        
+        stdManager.studentsByGroup();
+        stdManager.removeStudent("17020853");
+        System.out.println("-------After remove-------");
+        stdManager.studentsByGroup();
         
     }
     
