dc1dd83ee0ef68cf6de750e7e5cb63b089617a5f
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-09-20 02:22:01 +0700

    Lab03

---
+++ Lab03/src/Ex12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex12 {
+    //Cau a viet ham tinh UCLN cua a va b
+    public int gcd(int a, int b) {
+        int i = (b == 0) ? a : gcd(b, a % b);
+        return i;
+    }
+    // Cau b viet ham tra ve só fibonacci thu n
+    public int fibonacci(int n) {
+        if (n<=1) return n;
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        int a,b,n;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        n = scan.nextInt();
+
+        Ex12 x = new Ex12();
+
+        System.out.println("Uoc chung lon nhat cua hai so " + a + "," + b + " la:" +x.gcd(a,b));
+        System.out.println("So fibonacci thu " + n + " la:" +x.fibonacci(n));
+
+    }
+}
---
+++ Lab03/src/PS.java
@@ -0,0 +1,11 @@
+public class PS {
+    private int tu;
+    private int mau;
+
+    public PS(int tu, int mau) {
+
+        this.tu = tu;
+        this.mau = mau;
+    }
+    
+}
