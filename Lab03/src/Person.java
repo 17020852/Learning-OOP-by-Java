5f20b3ca758f131de362a3f114aec767b679eba7
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-09-24 14:46:40 +0700

    creat class persion

---
+++ Lab03/src/Person.java
@@ -0,0 +1,55 @@
+public class Person {
+    private int age;
+    private String name;
+    private String address;
+    private float height;
+    private float weight;
+
+    public Person(int age, String name, String address, float height, float weight) {
+        this.age = age;
+        this.name = name;
+        this.address = address;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
