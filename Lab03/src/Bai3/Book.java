ee7a1a916907e0c1c20c689fdbcd3d604a2c8a32
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-09-24 15:52:04 +0700

    Book class

---
+++ Lab03/src/Bai3/Book.java
@@ -0,0 +1,57 @@
+package Bai3;
+
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+    private int year;
+    private int numberpage;
+
+    public Book() {
+        this.title="title";
+        this.author="author";
+        this.price=0;
+        this.year=0;
+        this.numberpage = 0;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setNumberpage(int numberpage) {
+        this.numberpage = numberpage;
+    }
+
+    public int getNumberpage() {
+        return numberpage;
+    }
+}
