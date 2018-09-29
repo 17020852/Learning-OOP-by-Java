dabaddac632753cbccc3b47105894e3c99eabe96
linhht-uet <hoanglinh831999@gmail.com>
2018-09-29 21:09:41 +0700

    update Lab04

--- Lab04/src/TestMaximum.java
+++ Lab04/src/TestMaximum.java
@@ -11,5 +11,17 @@ public class TestMaximum {
     public void TestMax02() {
         assertEquals(4, Lab04.FindMax(3,4));
     }
+    @Test
+    public void TestMax03() {
+        assertEquals(5, Lab04.FindMax(1, 5));
+    }
+    @Test
+    public void TestMax04() {
+        assertEquals(6, Lab04.FindMax(6, 2));
+    }
+    @Test
+    public void TestMax() {
+        assertEquals(7, Lab04.FindMax(4, 7));
+    }
 
 }
---
+++ Lab04/src/TestMinimum.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestMinimum {
+    @Test
+    public void TestMin_01() {
+        int[] arr01 = {100, 1, 12, 3, 4, 16};
+        assertEquals(1, Lab04.FindMin(arr01, 6));
+    }
+    @Test
+    public void TestMin_02() {
+        int[] arr02 = {-10, 11, 13, 14};
+        assertEquals(-10, Lab04.FindMin(arr02, 4));
+    }
+    @Test
+    public void TestMin_03() {
+        int[] arr03 = {1,2,3,4,5,6,7,-1};
+        assertEquals(-1,Lab04.FindMin(arr03, 8));
+    }
+    @Test
+    public void TestMin_04() {
+        int[] arr04 = {50, 20, 0, 25, 25, 46, 12};
+        assertEquals(0, Lab04.FindMin(arr04, 7));
+    }
+    @Test
+    public void TestMin_05() {
+        int[] arr05 = {-5,-4,-3,-2,-1,0,1,2,3,4,5};
+        assertEquals(-5, Lab04.FindMin(arr05, 11));
+    }
+}
