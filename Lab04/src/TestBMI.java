8146c6210523de8b46b26028a7dbd4da05e69bc8
linhht-uet <hoanglinh831999@gmail.com>
2018-10-01 13:17:56 +0700

    Update Lab04

--- Lab04/src/TestBMI.java
+++ Lab04/src/TestBMI.java
@@ -3,22 +3,27 @@ import static org.junit.Assert.*;
 public class TestBMI {
     @Test
     public void Test_case_01() {
-        assertEquals("Thieu can", Lab04.BMI(45, 1.6));
+        Lab04 test1 = new Lab04();
+        assertEquals("Thieu can", test1.BMI(45, 1.6));
     }
     @Test
     public void Test_case_02() {
-        assertEquals("Binh thuong", Lab04.BMI(50,1.6));
+        Lab04 test2 = new Lab04();
+        assertEquals("Binh thuong", test2.BMI(50,1.6));
     }
     @Test
     public void Test_case_03() {
-        assertEquals("Thua can", Lab04.BMI(60, 1.6));
+        Lab04 test3 = new Lab04();
+        assertEquals("Thua can", test3.BMI(60, 1.6));
     }
     @Test
     public void Test_case_04() {
-        assertEquals("Beo phi", Lab04.BMI(65, 1.6));
+        Lab04 test4 = new Lab04();
+        assertEquals("Beo phi", test4.BMI(65, 1.6));
     }
     @Test
     public void Test_case_05() {
-        assertEquals("Binh thuong", Lab04.BMI(52, 1.6));
+        Lab04 test5 = new Lab04();
+        assertEquals("Binh thuong", test5.BMI(52, 1.6));
     }
 }
