4e37f6ed2c0dd6f3fc9245ecb21a78dfe4994412
linhht-uet <hoanglinh831999@gmail.com>
2018-10-01 16:05:21 +0700

    Update Lab04

---
+++ Lab04/src/Week4.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Random.*;
+import java.util.Scanner;
+
+public class Week4 {
+    public static void main(String[] args) throws FileNotFoundException{
+        //cau 1
+        Random random = new Random();
+        int mark = random.nextInt(100);
+        int count = 0;
+        boolean check = false;
+        while (count <10 && !check) {
+            int x;
+            System.out.println("Enter a number:");
+            Scanner scanner = new Scanner(System.in);
+            x = scanner.nextInt();
+            if (x == mark) {
+                System.out.println("Correct!");
+                check = true;
+            }
+            else if (x<mark){
+                if (count <9) {
+                    System.out.println("Too low! Try again!");
+                }
+
+            }
+            else {
+                if (count<9) {
+                    System.out.println("Too high! Try again!");
+                }
+            }
+            count++;
+
+        }
+        if ( count == 10) {
+            System.out.println("You lose!\n The correct number is: " +mark +"\n" );
+        }
+        //Cau 2
+        int count2 =0;
+        File file = new File("lab04.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            scanner.next();
+            count2++;
+
+        }
+        System.out.println("So tu trong file la: " + count2);
+
+    }
+}
