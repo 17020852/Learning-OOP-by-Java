b095de07d2f0f875ad9bd42190603cea04fbd527
Hoang Thi Linh <hoanglinh831999@gmail.com>
2018-10-05 23:10:03 +0700

    Update TestBMI

--- Lab04/src/TestBMI.java
+++ Lab04/src/TestBMI.java
@@ -4,22 +4,22 @@ public class TestBMI {
     //Xac nhan rang thong bao ve can nang tinh theo chi so BMI chinh xac
     @Test
     public void Test_case_01() {
-        assertEquals("Thieu can", Lab04.BMI(45, 1.6));
+        assertEquals("Thieu can", Lab04.calculateBMI(45, 1.6));
     }
     @Test
     public void Test_case_02() {
-        assertEquals("Binh thuong", Lab04.BMI(50,1.6));
+        assertEquals("Binh thuong", Lab04.calculateBMI(50,1.6));
     }
     @Test
     public void Test_case_03() {
-        assertEquals("Thua can", Lab04.BMI(60, 1.6));
+        assertEquals("Thua can", Lab04.calculateBMI(60, 1.6));
     }
     @Test
     public void Test_case_04() {
-        assertEquals("Beo phi", Lab04.BMI(65, 1.6));
+        assertEquals("Beo phi", Lab04.calculateBMI(65, 1.6));
     }
     @Test
     public void Test_case_05() {
-        assertEquals("Binh thuong", Lab04.BMI(52, 1.6));
+        assertEquals("Binh thuong", Lab04.calculateBMI(52, 1.6));
     }
 }
